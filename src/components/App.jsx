1d3dd933cf7d91fa39632ad65c388e00a9eeb7fe
Anton Shabalov <shabalovanton@gmail.com>
2023-02-11 12:22:43 +0200

    fix modal and App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import { fetchImgs } from "js/fetchSearch"
 import { Message } from "./Message/Message";
 
 export const App = () => {
-  const [tagImg, setTagImg] = useState('2,71828182845904523536');
+  const [tagImg, setTagImg] = useState('');
   const [imgs, setImgs] = useState([]);
   const [status, setStatus] = useState('idel');
   const [page, setPage] = useState(1);
@@ -19,7 +19,7 @@ export const App = () => {
   const [showImg, setShowImg] = useState(null);
 
   useEffect(() => {
-    if (tagImg === '2,71828182845904523536') {
+    if (tagImg === '') {
       return
     }
     setStatus('pending');
@@ -28,14 +28,9 @@ export const App = () => {
       if (!imgs.total) {
         return setStatus('absent')
       }
-      if (Math.ceil(imgs.totalHits / 12) === page) {
-        setImgs(state => [...state, ...imgs.hits]) 
-        setStatus('end') 
-        return 
-      }
       setImgs(state => [...state, ...imgs.hits])
-      setStatus('resolved') 
-      return })
+      setStatus( Math.ceil(imgs.totalHits / 12) === page ? 'end' : 'resolved')
+      })
     .catch(error => {
       setError(error);
       setStatus('rejected')}
@@ -85,76 +80,4 @@ export const App = () => {
   );
 };
 
-// export class App extends Component {
-//   state = {
-//     tagImg: null,
-//     imgs: [],
-//     status: 'idel',
-//     page: 1,
-//     error: null,
-//     showModal: false,
-//     showImg: null
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevState.tagImg !== this.state.tagImg || prevState.page !== this.state.page) {
-//       this.setState({status: 'pending'})
-//       fetchImgs(this.state.tagImg, this.state.page)
-//       .then(imgs => this.setState(prevState => {
-//         if (!imgs.total) {
-//           return {status: 'absent'} 
-//         }
-//         if (Math.ceil(imgs.totalHits / 12) === this.state.page) {
-//           return {imgs: [...prevState.imgs, ...imgs.hits], status: 'end'}
-//         }
-//         return {imgs: [...prevState.imgs, ...imgs.hits], status: 'resolved'}}))
-//       .catch(error => this.setState({error, status: 'rejected'}))
-//     }
-//   }
-
-//   loadMore = () => {
-//     this.setState(({page}) => {return {page: page+1}})
-//   }
-
-//   toggleModal = () => {
-//     this.setState(({showModal}) => ({showModal: !showModal}))
-//   }
-
-//   handelFormSubmit = (tagImg) => {
-//     if (tagImg === this.state.tagImg) {
-//       return toast.error('Це ми вже знайшли 🙃')
-//     }
-//     this.setState({tagImg: tagImg, page: 1, imgs: []})
-//   }
-
-//   openModal = (img) => {
-//     this.toggleModal()
-//     this.setState({showImg: img})
-//   }
-
-//   closeModal = () => {
-//     this.toggleModal()
-//   }
-
-//   render () {
-
-//     const {imgs, showModal, status, tagImg, showImg} = this.state
-
-//     return (
-//       <>
-//         <Searchbar onSubmit = {this.handelFormSubmit}/>
-//         {imgs && <ImageGallery imgs ={imgs} onOpenModal={this.openModal}/>}
-//         {showModal && <Modal img = {showImg} onClose = {this.closeModal}/>}
-//         {status === 'pending' && <Loader />}
-//         {status === 'resolved' && <Button onClick={this.loadMore}/>}
-//         {status === 'end' && <Message><h2>Це всі зображення, що знайшли 🙃</h2></Message>}
-//         {status === 'absent' && <Message><h2>No results found for "{tagImg}".</h2></Message>}
-//         {status === 'rejected' && <Message><h2>ERROR</h2></Message>}
-
-//         <Toaster position="bottom-center"/>
-//       </>
-//     );
-//   }
-// };
-
-// 'resolved' 'pendingMore' 'idel' 'pendingMore' 'end'
+// 'idel' 'resolved' 'pending' 'end' 'absent' 'rejected'
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,17 +6,16 @@ import PropTypes from 'prop-types';
 const modalRoot = document.querySelector('#root-modal');
 
 export const Modal = ({img, onClose}) => {
-  
+
   useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === 'Escape') {
+        onClose()
+      }
+    }
     window.addEventListener('keydown', handleKeyDown)
     return () => {window.removeEventListener('keydown', handleKeyDown)}
-  }, )
-
-  const handleKeyDown = (e) => {
-    if (e.code === 'Escape') {
-      onClose()
-    }
-  }
+  }, [onClose])
 
   const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
@@ -38,42 +37,4 @@ Modal.propTypes = {
   img: PropTypes.object.isRequired,
   onClose: PropTypes.func.isRequired,
 }
-
-
-
-// export class Modal extends Component {
-  
-//   componentDidMount () {
-//     window.addEventListener('keydown', this.handleKeyDown)
-//   }
-
-//   componentWillUnmount () {
-//     window.removeEventListener('keydown', this.handleKeyDown)
-//   }
-
-//   handleKeyDown = (e) => {
-//     if (e.code === 'Escape') {
-//       this.props.onClose()
-//     }
-//   }
-
-//   handleBackdropClick = (e) => {
-//     if (e.currentTarget === e.target) {
-//       this.props.onClose()
-//     }
-//   }
-
-//   render () {
-//     const img = this.props.img
-//     return createPortal(
-//       <Overlay onClick={this.handleBackdropClick}>
-//         <Window>
-//           <img src={img.largeImageURL} alt={img.tags} />
-//         </Window>
-//       </Overlay>
-//       ,modalRoot
-//     )
-//   }
-  
-// }
 
